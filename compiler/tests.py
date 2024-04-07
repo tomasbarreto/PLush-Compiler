112e719e730d7886b851faac1d047f111e59aa60
Tomás Barreto <65412084+TomasBarreto@users.noreply.github.com>
2024-04-07 06:40:10 +0100

    add: tests for the tokenizer

---
+++ compiler/tests.py
@@ -0,0 +1,16 @@
+from tokenizer import tokenize
+
+source_code = '''
+# This is a comment
+val x = 'ola'
+var y = 10 # This is a comment
+if x > y {
+    y = y + 1
+} then else { # This is a comment
+    y = y - 1
+} 
+# This is a comment
+'''
+tokens = tokenize(source_code)
+for token in tokens:
+    print(token)
