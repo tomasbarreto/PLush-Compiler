babb179f94a1f0446037c2cd31e58731ce9b29be
Tomás Barreto <65412084+TomasBarreto@users.noreply.github.com>
2024-04-07 06:56:52 +0100

    update: tokenizer tests

--- compiler/tests.py
+++ compiler/tests.py
@@ -2,12 +2,12 @@ from tokenizer import tokenize
 
 source_code = '''
 # This is a comment
-val x = 'ola'
-var y = 10 # This is a comment
+val x := 'ola'
+var y := 10 # This is a comment
 if x > y {
-    y = y + 1
+    y := y + 1
 } then else { # This is a comment
-    y = y - 1
+    y := y - 1
 } 
 # This is a comment
 '''
