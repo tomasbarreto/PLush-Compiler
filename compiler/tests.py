8c21b04cb1cda59087c2aef6debbc5bff1058085
Tomás Barreto <65412084+TomasBarreto@users.noreply.github.com>
2024-04-08 07:44:17 +0100

    update: tests updates

--- compiler/tests.py
+++ compiler/tests.py
@@ -1,16 +1,26 @@
 from tokenizer import tokenize
+from plush_parser import parse
 
 source_code = '''
-# This is a comment
-val x := 'ola'
-var y := 10 # This is a comment
-if x > y {
-    y := y + 1
-} then else { # This is a comment
-    y := y - 1
-} 
-# This is a comment
+val x : [int] := false && true || false = false;
+
+while true {
+    var y : int := kekw()[1][2][3];
+}
+
+print("fhgdasfdsa");
+
+if true {
+    x := print("Hello, World!")[1];
+    print(x);
+}
+
+function kekw() : [int] {
+    return getArray();
+}
 '''
 tokens = tokenize(source_code)
 for token in tokens:
     print(token)
+
+parse(tokens)
