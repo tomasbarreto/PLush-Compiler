763370e5df5d212bffd9e4840958e125402754e3
Tomas Barreto <fc56282@alunos.fc.ul.pt>
2024-05-02 09:31:51 +0100

    refactor: interpretor function rename

--- compiler/plush_interpretor.py
+++ compiler/plush_interpretor.py
@@ -1,39 +1,39 @@
 from context import Context
 from plush_ast import *
 
-def plush_interpretor(node, ctx: Context):
+def interpretor(node, ctx: Context):
     if isinstance(node, Program):
-        plush_interpretor(node.statements, ctx)
+        interpretor(node.statements, ctx)
     elif isinstance(node, InstructionList):
         for instruction in node.instructions:
-            plush_interpretor(instruction, ctx)
+            interpretor(instruction, ctx)
     elif isinstance(node, FunctionDeclaration):
-        plush_interpretor(node.instructions, ctx)
+        interpretor(node.instructions, ctx)
     elif isinstance(node, VariableDeclaration):
         if ctx.has_var_in_current_scope(node.name):
             raise TypeError(f"Variable {node.name} already declared!")
         
-        ctx.set_type(node.name, plush_interpretor(node.value, ctx))
+        ctx.set_type(node.name, interpretor(node.value, ctx))
     elif isinstance(node, Expression):
-        return plush_interpretor(node.expr, ctx)
+        return interpretor(node.expr, ctx)
     elif isinstance(node, Add):
-        return plush_interpretor(node.left, ctx) + plush_interpretor(node.right, ctx)
+        return interpretor(node.left, ctx) + interpretor(node.right, ctx)
     elif isinstance(node, Sub):
-        return plush_interpretor(node.left, ctx) - plush_interpretor(node.right, ctx)
+        return interpretor(node.left, ctx) - interpretor(node.right, ctx)
     elif isinstance(node, Mult):
         if node.operator == "*":
-            return plush_interpretor(node.left, ctx) * plush_interpretor(node.right, ctx)
+            return interpretor(node.left, ctx) * interpretor(node.right, ctx)
         elif node.operator == "/":
-            return plush_interpretor(node.left, ctx) / plush_interpretor(node.right, ctx)
+            return interpretor(node.left, ctx) / interpretor(node.right, ctx)
         else:
-            return plush_interpretor(node.left, ctx) ** plush_interpretor(node.right, ctx)
+            return interpretor(node.left, ctx) ** interpretor(node.right, ctx)
     elif isinstance(node, Unary):
         if node.operator == "+":
-            return plush_interpretor(node.expr, ctx)
+            return interpretor(node.expr, ctx)
         elif node.operator == "-":
-            return -plush_interpretor(node.expr, ctx)
+            return -interpretor(node.expr, ctx)
         else:
-            return not plush_interpretor(node.expr, ctx)
+            return not interpretor(node.expr, ctx)
     elif isinstance(node, Int):
         return int(node.value)
     elif isinstance(node, Float):
