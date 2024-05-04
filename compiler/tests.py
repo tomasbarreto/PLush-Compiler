b2f772c03225a42d9198751be85183fb775ca42a
Tomás Barreto <tomasalexandrebarreto@gmail.com>
2024-05-04 08:32:02 +0100

    update: tests

--- compiler/tests.py
+++ compiler/tests.py
@@ -1,12 +1,28 @@
-from tokenizer import tokenize
+from plush_tokenizer import tokenize
 from plush_parser import parse
+from plush_typechecker import verify
+from plush_compiler import compile
+from context import Context
+from emitter import Emitter
 
 source_code = '''
-a := 2 * 2 - 3 * 2;
+var x : int := 1;
+var y : int := 1 + 2;
 
+var z : int := x + y;
+
+function print_int(var x : int) : void;
 '''
 tokens = tokenize(source_code)
 for token in tokens:
     print(token)
 
-parse(tokens)
+ast = parse(tokens)
+
+# type check the ast
+typed_ast = verify(ast, Context())
+
+llvm_code = compile(typed_ast, Emitter())
+
+for line in llvm_code:
+    print(line)
